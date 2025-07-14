6b64e840220ac30c118d559e69f85832e2139ea3
220-22\LeeEunJin <lee0104786@gmail.com>
2025-07-14 12:09:09 +0900

    test

--- src/test/java/org/mbc/mapper/BoardMapperTests.java
+++ src/test/java/org/mbc/mapper/BoardMapperTests.java
@@ -14,71 +14,66 @@ import lombok.extern.log4j.Log4j2;
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j2
 public class BoardMapperTests {
-	// board에서 사용하는 데이터베이스 테스트용
-	
+
 	@Setter(onMethod_ = @Autowired)
-	private BoardMapper mapper; // 인터페이스로 만든 객체를 세터로 연결
+	private BoardMapper mapper;
 	
-	@Test //import org.junit.Test; 메서드 단위로 테스트
+	@Test
 	public void testGetList() {
 		
 		mapper.getList().forEach(board -> log.info(board));
-		// 인터페이스.메서드.for문  결과객체
-		//                             -> 람다식(인터페이스용)
-		//                                log.info 콘솔에 출력(board.toString)
+		
 	}
 	
-	
-	@Test //import org.junit.Test; 메서드 단위로 테스트
+	@Test
 	public void testGetListXML() {
 		
 		mapper.getList2().forEach(board -> log.info(board));
-		// 인터페이스.메서드.for문  결과객체
-		//                             -> 람다식(인터페이스용)
-		//                                log.info 콘솔에 출력(board.toString)
 	}
 	
 	@Test
 	public void testInsert() {
 		
 		BoardVO board = new BoardVO();
-		board.setTitle("제발 오류 없어라");
-		board.setContent("이번엔 누구냐");
-		board.setWriter("김기원");
+		board.setTitle("너무 어려워요");
+		board.setContent("오류가 왜 뜨는지 모르겠어요");
+		board.setWriter("이은진");
 		
 		mapper.insert(board);
 		
 		log.info(board);
-		
 	}
 	
 	@Test
 	public void testInsertSelectKey() {
 		
 		BoardVO board = new BoardVO();
-		board.setTitle("이번엔 제발 번호 나와라");
-		board.setContent("내 게시물에 번호는? ");
-		board.setWriter("김기원");
+		board.setTitle("오타 좀 주의하자");
+		board.setContent("내 게시물의 번호는?");
+		board.setWriter("이은진");
 		
 		mapper.insertSelectKey(board);
 		
 		log.info(board);
 		log.info("내가만든 게시물의 번호는 : " + board.getBno());
+		
 	}
 	
 	@Test
 	public void testRead() {
 		
-		BoardVO board = mapper.read(1L); // 1번 게시물을 read 메서드로 보내고 객체로 받는다.
+		BoardVO board = mapper.read(1L);
 		
 		log.info(board);
+		
 	}
 	
 	@Test
 	public void testDelete() {
 		
-		int count = mapper.delete(3L); 
+		int count = mapper.delete(3L);
 		log.info("삭제된 갯수 출력 : " + count + "건");
+		
 	}
 	
 	@Test
@@ -88,14 +83,11 @@ public class BoardMapperTests {
 		board.setBno(5L);
 		board.setTitle("수정된 제목");
 		board.setContent("수정된 내용");
-		board.setWriter("김수정");
+		board.setWriter("이수정");
 		
 		int count = mapper.update(board);
 		log.info("수정된 갯수 : " + count + "건");
-		log.info("수정된 객체 출력 : " + board);
-		
-		
-		
+		log.info("수정도니 객체 출력 : " + board);
 		
 	}
 	
